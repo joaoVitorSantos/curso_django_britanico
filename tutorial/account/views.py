f1b6b9e506e8d9e002644a1e651899fa39a6d958
joaoVitorSantos <joaovitorjec@gmail.com>
2019-03-19 19:52:33 -0300

    First Commit

---
+++ tutorial/account/views.py
@@ -0,0 +1,65 @@
+from django.shortcuts import render, redirect
+from account.forms import RegistrationForm, EditProfileForm
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserChangeForm, PasswordChangeForm
+from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth.decorators import login_required
+
+# Create your views here.
+from django.urls import include
+
+
+def home(request):
+    numbers = [1, 2, 3, 4, 5]
+    name = 'João Vitor'
+    args = {'name': name, 'numbers': numbers}
+    return render(request, 'account/home.html', args)
+
+
+def register(request):
+    if request.method == 'POST':
+        form = EditProfileForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/account')
+    else:
+        form = RegistrationForm()
+        args = {'form': form}
+        return render(request, 'account/reg_form.html', args)
+
+@login_required()
+def profile(request):
+    args = {'user': request.user}
+    return render(request, 'account/profile.html', args)
+
+@login_required()
+def edit_profile(request):
+    if request.method == 'POST':
+        form = EditProfileForm(request.POST, instance=request.user)
+
+        if form.is_valid():
+            form.save()
+            return redirect('/account/profile')
+
+    else:
+        form = EditProfileForm(instance=request.user)
+        args = {'form': form}
+        return render(request, 'account/edit_profile.html', args)
+
+@login_required()
+def change_password(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(data=request.POST, user=request.user)
+
+        if form.is_valid():
+            form.save()
+            update_session_auth_hash(request, form.user)
+            return redirect('/account/profile')
+        else:
+            return redirect('/account/change-password')
+
+    else:
+        form = PasswordChangeForm(user=request.user)
+        args = {'form': form}
+
+        return render(request, 'account/change_password.html', args)
